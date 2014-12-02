8f0ea2c6860054b3ec0c76aa3dea101f78139275
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-12-02 15:27:06 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,7 +44,6 @@ class role_snmp {
   # Extract tar file using camptocamp/puppet-archive module
   archive { 'mibs-Oct2014':
     ensure   => present,
-    #url      => 'https://github.com/naturalis/puppet-role_snmp/raw/master/files/mibs-Oct2014.tar.gz',
     url      => 'https://raw.githubusercontent.com/naturalis/puppet-role_snmp/master/files/mibs-Oct2014.tar.gz',
     target   => '/usr/local/share/snmp/mibs',
     checksum => false,
