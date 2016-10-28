92ec13f0e4653307f013b0f620cad5bfa0dea72e
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-10-28 15:24:50 +0200

    Update snmpget.py

--- files/snmpget.py
+++ files/snmpget.py
@@ -4,7 +4,7 @@ import subprocess
 import yaml
 import json
 
-with open('data.yml') as yml_file:
+with open('/usr/local/bin/data.yml') as yml_file:
     config = yaml.load(yml_file)
 
 for device_config in config['devices']:
@@ -14,11 +14,12 @@ for device_config in config['devices']:
     for metric_config in device_config['metrics']:
         oid = metric_config['oid']
         description = metric_config['description']
+        units = metric_config['units']
         p = subprocess.Popen(['snmpget','-v','2c','-c',community,'-Ln','-OUt',host,oid],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
         out, err = p.communicate()
-        obj = {'host': host, 'description': description, 'devicegroup': devicegroup, 'result': out.rstrip('\n')}
+        obj = {'host': host, 'description': description, 'devicegroup': devicegroup, 'units': units, 'result': out.rstrip('\n')}
 #       print(json.dumps(obj, separators=(', ',': ')))
         with open("/var/log/snmp", "a") as out:
             out.write(json.dumps(obj, separators=(', ',': '))+'\n')
